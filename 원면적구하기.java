845eb630c13cec128f711d2742f2ef08811eca9c
yeongmin1231 <152240297+yeongmin1231@users.noreply.github.com>
2024-11-06 16:26:32 +0900

    Add files via upload

---
+++ 원면적구하기.java
@@ -0,0 +1,10 @@
+package sec01.exam03;
+
+public class ColmputerExample {
+    public static void main(String[] args) {
+        int r = 10;
+
+        Calculator calculator = new Calculator();
+        System.out.println("원면적 : " computer.areaCircle(r));
+    }
+}
