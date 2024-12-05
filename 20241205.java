4e7b93a52f8df25067815c92a23041272bdf1cd9
yeongmin1231 <152240297+yeongmin1231@users.noreply.github.com>
2024-12-05 14:24:14 +0900

    Add files via upload

---
+++ 20241205.java
@@ -0,0 +1,15 @@
+package ex01;
+
+public class Main {
+    public static void main (string[] args) {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        for(int i = 0; i < 5; i ++) {
+            toolkit.beep();
+            System.out.println("TTING");
+        }
+
+        for(int i = 0; i < 5; i++) {
+            System.out.println("DDONG")
+        }
+    }
+}
