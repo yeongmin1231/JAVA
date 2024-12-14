c6521c59e29a019a79b14f4a0d4bc04bfde62820
yeongmin1231 <152240297+yeongmin1231@users.noreply.github.com>
2024-12-14 10:51:23 +0900

    Add files via upload

---
+++ 20241214.java
@@ -0,0 +1,22 @@
+package sec02. exam02;
+
+public class Window {
+    Button button1 = new Button();
+    Button button2 = new Button();
+
+    Button.onClickListener listener = new Button.OnClickListener() {
+        @Override
+        public void onClick() {
+            System.out.println("전화를 겁니다. ");
+        }
+    };
+    Window() {
+        button1.setOnClickListener( listener );
+        button2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick() {
+                System.out.println("메시지를 보냅니다. ");
+            }
+        });
+    }
+}
